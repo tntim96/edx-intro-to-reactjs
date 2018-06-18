0b1925e25f148f3f795f89629136cbce2da9e212
tntim96 <tntim96@gmail.com>
2018-06-18 13:44:47 +1000

    Module 3 lab - add restrictions

--- module-03/lab.js
+++ module-03/lab.js
@@ -79,8 +79,29 @@ function SelectAndLabel(props) {
   );
 }
 
+function CheckBoxesAndLabel(props) {
+  var checkboxes = props.restrictions.map((item,index) => (
+    <div key={index}>
+      <label>
+        <input type="checkbox" name={props.name} value={item.value} onChange={props.handleChange}/> {item.description}
+      </label>
+    </div>
+  ));
+  return (
+      <div>
+      {props.label}<br/>
+        {checkboxes}
+      </div>
+  );
+}
+
 class App extends React.Component {
-  static activities = ['Science Lab','Swimming','Science Lab','Swimming','Painting'];
+  static activities = ['Science Lab','Swimming','Cooking','Painting'];
+  static restrictions = [
+    {value: 'a', description: 'Dietary Restrictions'},
+    {value: 'b', description: 'Physical Disabilities'},
+    {value: 'c', description: 'Medical Needs'}
+  ];
 
   constructor(props) {
     super(props);
@@ -88,11 +109,13 @@ class App extends React.Component {
       firstName: ""
       , lastName: ""
       , activity: App.activities[0]
+      , restrictions: []
       , items: []
     };
     this.handleFirstNameChange = this.handleFirstNameChange.bind(this)
     this.handleLastNameChange = this.handleLastNameChange.bind(this)
     this.handleActivityChange = this.handleActivityChange.bind(this)
+    this.handleRestrictionsChange = this.handleRestrictionsChange.bind(this)
   }
   handleFirstNameChange(event) {
     this.setState({ firstName: event.target.value });
@@ -103,14 +126,33 @@ class App extends React.Component {
   handleActivityChange(event) {
     this.setState({ activity: event.target.value });
   }
+  handleRestrictionsChange(event) {
+    debugger;
+    var restrictionsCopy = this.state.restrictions.slice();
+    if (event.target.checked) {
+      restrictionsCopy.push(event.target.value)
+    } else {
+      var index = restrictionsCopy.indexOf(event.target.value);
+      if (index > -1) {//It should be
+      restrictionsCopy.splice(index, 1)
+      }
+    }
+    restrictionsCopy.sort()
+    //console.log('handleRestrictionsChange', event.target.checked, event.target.value);
+    // console.log('restrictionsCopy', restrictionsCopy);
+    this.setState({ restrictions: restrictionsCopy });
+  }
   addItem() {
     var itemsCopy = this.state.items.slice();
+    if (this.state.firstName === '' || this.state.lastName === '')
+      return
     itemsCopy.push({
       firstName: this.state.firstName,
       lastName: this.state.lastName,
-      activity: this.state.activity
+      activity: this.state.activity,
+      restrictions: this.state.restrictions
     });
-    this.setState({ items: itemsCopy, firstName: "", lastName: "" , activity: App.activities[0] });
+    this.setState({ items: itemsCopy, firstName: "", lastName: "" , activity: App.activities[0], restrictions: [] });
     console.log('state', this.state)
   }
 
@@ -120,6 +162,7 @@ class App extends React.Component {
         <InputAndLabel label="First Name" value={this.state.firstName} handleChange={this.handleFirstNameChange}/>
         <InputAndLabel label="Last Name" value={this.state.lastName} handleChange={this.handleLastNameChange}/>
         <SelectAndLabel label="Select Activity" value={this.state.activity} handleChange={this.handleActivityChange} activities={App.activities}/>
+        <CheckBoxesAndLabel label="Check all that apply:" name="restrictions" value={this.state.restrictions} handleChange={this.handleRestrictionsChange} restrictions={App.restrictions}/>
         <button onClick={() => this.addItem()}>Submit</button><br/>
         <Table tableList={this.state.items}/>
       </div>
